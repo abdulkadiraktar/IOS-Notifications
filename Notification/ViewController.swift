6a8a68ae1733ae66a13c0751655b639980790a8c
Abdulkadir Aktar <aktarkadir1@gmail.com>
2024-05-21 19:32:48 +0300

    Initial commit

--- Notification/ViewController.swift
+++ Notification/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import UserNotifications
+
 
 class ViewController: UIViewController {
-
+    
+    var permissionCheck = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //bildirim izin isteme
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge], completionHandler:{ granted, error in
+            self.permissionCheck = granted
+            
+            
+        })
+        UNUserNotificationCenter.current().delegate = self    }
+
+    @IBAction func buttonNotification(_ sender: Any) {
+        if permissionCheck {
+            let content = UNMutableNotificationContent()
+            content.title = "Başlık"
+            content.subtitle = "Alt Başlık"
+            content.body = "İçerik"
+            content.badge = 1
+            content.sound = UNNotificationSound.default
+            
+            //ne zaman tetiklenecek  //timeInterval 10 saniye sonra bildirim gözükecek çünkü uygulama açıkken bildirim gözükmez
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+            let request = UNNotificationRequest(identifier: "id", content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request)
+        }
     }
-
-
+    
+}
+// uygulama çalışırken de bildirim gelebilmesi için
+extension ViewController : UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner,.sound,.badge])
+    }
+    //bildirim seçildiğinde
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let app = UIApplication.shared
+        
+        if app.applicationState == .active {
+            print("Önplan : Bildirim Seçildi")
+        }
+        if app.applicationState == .inactive {
+            print("Arkaplan : Bildirim Seçildi")
+        }
+        
+        //app.applicationIconBadgeNumber = 0 güncellemeden önceki hali
+        center.setBadgeCount(0)
+        
+        print("Bildirim seçildi")
+        completionHandler()
+    }
 }
 
